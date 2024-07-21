935c3121da6d1a5eff1e3cac483de1cb61f1e91d
naaji-navas <najidnavas2000@gmail.com>
2024-07-21 15:48:22 +0530

    learn: added the search in a string

---
+++ src/com/naaji/lenearsearch/searchinstring.java
@@ -0,0 +1,24 @@
+package com.naaji.lenearsearch;
+
+public class searchinstring {
+    public static void main(String[] args) {
+        String name = "Naaji";
+        char target = 'p';
+        System.out.println(search(name, target));
+    }
+
+    static boolean search(String str ,char target){
+        if(str.length()==0){
+            return false;
+        }
+        for (int i =0; i<str.length();i++){
+            if(target==str.charAt(i)){
+                return true;
+
+            }
+
+        }
+        return false;
+
+    }
+}
